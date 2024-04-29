967a093583975599eb1d1b9443cf790b0198f482
Kyungchae <jkc4416@gmail.com>
2024-04-30 08:11:46 +0900

    240430_Add files

--- programming_languages/cpp/book/luxury_cpp_programming/Chapter4/Examples/ex4-8/ex4-8.cpp
+++ programming_languages/cpp/book/luxury_cpp_programming/Chapter4/Examples/ex4-8/ex4-8.cpp
@@ -8,11 +8,11 @@ private:
     int radius;
 // public 접근 제어자를 가지는 생성자 및 멤버 함수 선언
 public:
-    Circle();  // 매개변수가 없는 기본 생성자 선언
-    Circle(int r);  // 매개변수가 있는 생성자 선언
-    ~Circle();  // 소멸자 선언
-    void setRadius(int r);  // 멤버 함수 선언
-    double getArea();  // 멤버 함수 선언
+    Circle();
+    Circle(int r);
+    ~Circle();
+    void setRadius(int r);
+    double getArea();
 };
 
 // Circle 클래스 구현부
@@ -27,7 +27,6 @@ Circle::Circle(int r) {
 Circle::~Circle() {
     cout << "소멸자 실행 radius = " << radius << endl;
 }
-
 // Circle 클래스의 setRadius 멤버 함수 구현
 void Circle::setRadius(int r) {
     radius = r;
@@ -39,10 +38,15 @@ double Circle::getArea() {
 
 // main 함수 정의
 int main() {
-    Circle *p, *q;
-    p = new Circle;
-    q = new Circle(30);
-    cout << p->getArea() << endl << q->getArea() << endl;
-    delete p;
-    delete q;
+    int radius;
+    while(true) {
+        cout << "정수 반지름 입력(음수이면 종료)>> ";
+        cin >> radius;
+        if (radius < 0) {
+            break;
+        }
+        Circle *p = new Circle(radius);
+        cout << "원의 면적은 " << p->getArea() << endl;
+        delete p;
+    }
 }
