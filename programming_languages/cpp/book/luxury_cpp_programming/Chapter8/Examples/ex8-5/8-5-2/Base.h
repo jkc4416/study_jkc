ca09cb24d696f3c7cb62413ecbf10a24d9a2b8a8
Kyungchae Jung - MacBookPro <jkc4416@gmail.com>
2024-06-23 19:25:09 +0900

    240623_Add files

---
+++ programming_languages/cpp/book/luxury_cpp_programming/Chapter8/Examples/ex8-5/8-5-2/Base.h
@@ -0,0 +1,17 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <iostream>
+using namespace std;
+
+// Base 클래스 선언
+class Base {
+private:
+    int a;  // private 멤버 변수로서 외부 접근 불가
+protected:
+    void setA(int a);  // protected 멤버 함수로서 파생 클래스에서 접근 가능
+public:
+    void showA();  // public 멤버 함수로서 누구나 접근 가능
+};
+
+#endif
---
+++ programming_languages/cpp/book/luxury_cpp_programming/Chapter8/Examples/ex8-5/8-5-2/main.cpp
@@ -0,0 +1,16 @@
+#include "Derived.h"
+
+int main() {
+    Derived x;  // Derived 클래스 객체 생성
+
+    // 다음 줄의 코드들은 모두 컴파일 오류 발생
+    // x.a = 5;  // private 멤버 a에 접근 불가
+    // x.setA(10);  // protected 상속으로 인해 setA()에 접근 불가
+    // x.showA();  // protected 상속으로 인해 showA()에 접근 불가
+
+    // x.b = 10;  // private 멤버 b에 접근 불가
+    // x.setB(10);  // protected 멤버 함수 setB()에 접근 불가
+
+    x.showB();  // public 멤버 함수 showB() 호출, 출력 가능
+    return 0;  // 프로그램 종료
+}
