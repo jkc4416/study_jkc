4f4076f73767abea1a8a922688fa3d7ed5b18fcd
Kyungchae Jung - MacBookPro <jkc4416@gmail.com>
2024-06-23 21:22:08 +0900

    240623_Add files

--- programming_languages/cpp/book/luxury_cpp_programming/Chapter8/OpenChallenge/oc-2/Product.cpp
+++ programming_languages/cpp/book/luxury_cpp_programming/Chapter8/OpenChallenge/oc-2/Product.cpp
@@ -7,7 +7,7 @@ int Product::id_counter = 0;
 // 생성자 정의
 Product::Product(const string& desc, const string& prod, int price)
     : description(desc), producer(prod), price(price) {
-    id = id_counter++; // 상품 식별자 자동 할당
+    id = id_counter++; // 상품 식별자 자동 할당 (Product 클래스의 이 생성자가 실행될 때마다 id_counter 정적 변수의 값을 1씩 증가시켜 id 인스턴스 변수에 저장)
 }
 
 // 상품 정보를 출력하는 함수 정의
